fdd446835a39249ad40d2ddcb51f4a669724081d
Aaron Strong <awstrong@gmail.com>
2023-01-23 15:37:21 -0600

    Update web-server-instance.tf

--- labs/gcp-hybrid-bgp-vpn/web-server-instance.tf
+++ labs/gcp-hybrid-bgp-vpn/web-server-instance.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "apache" {
   tags         = ["allow-internal"]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
       size  = 10
       type  = "pd-standard"
     }
